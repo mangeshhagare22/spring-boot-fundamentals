8edccc450a8eb718b7776194e1b17dee9271ac50
mangesh.hagare22 <mangesh.hagare20@gmail.com>
2024-09-02 20:32:18 +0530

    created REST Client : ref- src/test/java - ProductrestclientApplicationTests.java performed JUnit test cases

---
+++ project5/productrestclient/src/main/java/com/mangesh/restclient/ProductrestclientApplication.java
@@ -0,0 +1,13 @@
+package com.mangesh.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductrestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductrestclientApplication.class, args);
+	}
+
+}
