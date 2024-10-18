02428fa1e7750d0cce4c05b97cb13974bddfd6d4
mangesh.hagare22 <mangesh.hagare20@gmail.com>
2024-10-18 21:06:44 +0530

    Validations

---
+++ project_17/productrestapi/src/main/java/com/mangesh/springweb/entities/Product.java
@@ -0,0 +1,61 @@
+package com.mangesh.springweb.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	private String name;
+	@Size(max = 100)
+	private String description;
+	@Min(value = 1, message="The minimum price should be 1")
+	private int price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
---
+++ project_17/productrestapi/src/test/java/com/mangesh/springweb/ProductrestapiApplicationTests.java
@@ -0,0 +1,51 @@
+package com.mangesh.springweb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import com.mangesh.springweb.entities.Product;
+
+
+@SpringBootTest
+class ProductrestapiApplicationTests {
+
+	@Test
+	public void testGetProduct() {
+
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
+		assertNotNull(product);
+		assertEquals("IPhone",product.getName());
+	}
+	
+	@Test
+	public void testCreateProduct()
+	{
+		RestTemplate restTemplate=new RestTemplate();
+		Product product=new Product();
+		product.setName("Samsung Mobile");
+		product.setDescription("Its Awesome");
+		product.setPrice(1000);
+		Product newProduct=restTemplate.postForObject("http://localhost:8080/productapi/products/", product,Product.class);
+		
+		assertNotNull(newProduct);
+		assertNotNull(newProduct.getId());
+		assertEquals("Samsung Mobile", newProduct.getName());
+	}
+	
+	@Test
+	public void testUpdateProduct() {
+
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
+		product.setPrice(1400);
+		restTemplate.put("http://localhost:8080/productapi/products/", product);
+	}
+
+
+}
